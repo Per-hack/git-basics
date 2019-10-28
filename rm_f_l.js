27ef0a799a9e5af032fd234de37c911f70d5e495
Per-hack <perise.k4@gmail.com>
2019-10-28 16:17:03 +0300

    drop first and last character from string
    in case you have more detailed explanation
    regarding the changes introduced do it
    in this paragraph

---
+++ rm_f_l.js
@@ -0,0 +1,6 @@
+function drop_first_last_char(str){
+    let str_arr = str.split('');
+return str_arr.filter((e, i) => !((i === 0) || (i === str_arr.length-1))).join('');
+}
+
+console.log(drop_first_last_char('Peris'));
